16b47c2cb0dde947eba6741b24e9142d44ada74b
Hedde van der Heide <me@heddevanderheide.nl>
2019-10-28 09:29:31 +0100

    overview k8s with traefik and python flask sample apps

--- sa-service-1/src/main.py
+++ sa-service-1/src/main.py
@@ -1,9 +1,22 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+import socket
+hostname = socket.gethostname()
+
 @app.route('/')
 def info():
-    return jsonify(name="Service 1")
+    return jsonify(
+        name="service-one",
+        host=hostname
+    )
+
+@app.route('/topic')
+def stock():
+    return jsonify(state={
+        "apples": 5,
+        "oranges": 7
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- sa-service-2/src/main.py
+++ sa-service-2/src/main.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+import socket
+hostname = socket.gethostname()
+
 @app.route('/')
 def info():
-    return jsonify(name="Service 2")
+    return jsonify(
+        name="service-two",
+        host=hostname
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
