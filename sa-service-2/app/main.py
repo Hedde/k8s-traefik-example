f08a2cfcff919588b68519757f6bf53077f847a9
Hedde van der Heide <me@heddevanderheide.nl>
2019-11-01 13:34:37 +0100

    remove redundant jsonresponse

--- sa-service-2/app/main.py
+++ sa-service-2/app/main.py
@@ -2,7 +2,6 @@ import socket
 import sys
 
 from fastapi import FastAPI
-from starlette.responses import JSONResponse
 
 app = FastAPI()
 
@@ -13,8 +12,8 @@ version = f"{sys.version_info.major}.{sys.version_info.minor}"
 
 @app.get("/")
 async def read_root():
-    return JSONResponse(content={
+    return {
         "name": "service-two",
         "host": hostname,
         "version": f"Hello world! From FastAPI running on Uvicorn with Gunicorn in Alpine. Using Python {version}"
-    })
+    }
